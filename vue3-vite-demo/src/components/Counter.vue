b82cae395a4ac4878847561d5ec716b0c43f17a8
jackluson <18219112108@163.com>
2021-10-09 22:52:47 +0800

    feat: add vue3-vite-demo

---
+++ vue3-vite-demo/src/components/Counter.vue
@@ -0,0 +1,39 @@
+<script lang="ts">
+import { defineComponent, ref } from "vue";
+
+export default defineComponent({
+  props: {
+    init: {
+      default: 0,
+      type: Number,
+    },
+  },
+  setup(props) {
+    const count = ref(props.init);
+
+    const increment = () => {
+      count.value++;
+    };
+    return {
+      count: count,
+      increment,
+    };
+  },
+  data() {
+    return { msg: "setup demo" };
+  },
+  mounted() {
+    setInterval(() => {
+      this.increment();
+    }, 1000);
+  },
+  methods: {
+    // increment() {
+    //   console.log("in");
+    //   this.count++;
+    // },
+  },
+});
+</script>
+
+<template>Counter: {{ count }}</template>
