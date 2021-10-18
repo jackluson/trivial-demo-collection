129f7eedb8e33130776e4f60700dcf782292e2f7
jackluson <18219112108@163.com>
2021-10-18 18:15:42 +0800

    feat: add vue reactive api demo

--- vue3-vite-demo/src/components/Counter.vue
+++ vue3-vite-demo/src/components/Counter.vue
@@ -1,5 +1,10 @@
+<!--
+ * @Date: 2021-10-12 10:45:32
+ * @LastEditTime: 2021-10-18 17:35:27
+ * @Description: 
+-->
 <script lang="ts">
-import { defineComponent, ref } from "vue";
+import { defineComponent, ref, watchEffect, reactive, watch } from "vue";
 
 export default defineComponent({
   props: {
@@ -14,20 +19,87 @@ export default defineComponent({
     const increment = () => {
       count.value++;
     };
+
+    watch(
+      ()=> count.value,
+      (count, prevCount) => {
+        console.log("🚀 ~ setup ~ count, prevCount", count, prevCount)
+        /* ... */
+      }
+    )
+
+    watchEffect((...rest) => {
+      console.log("🚀 ~ watchEffect ~ rest", rest)
+      /* 
+      count 会在初始运行时同步打印出来
+      更改 count 时，将在组件更新前执行副作用。
+       */
+      console.log('watchEffect', count.value)
+    }, 
+     {
+        flush: 'post' //  post值时, 更改 count 时，将在组件更新后执行副作用。
+      }
+    )
+
+    // 
+    const state = reactive({ 
+      id: 1,
+      attributes: { 
+        name: '',
+      }
+    })
+
+    const resWatch = watch(
+      () => state,
+      (state, prevState, onInvalidate) => {
+        onInvalidate(()=>{
+          console.log("🚀 ~ setup ~ onInvalidate", onInvalidate)
+        })
+        console.log("🚀 ~ setup ~ state, prevState", JSON.stringify(state, null, 2 ), JSON.stringify(prevState, null, 2 ))
+        console.log("🚀 ~ setup ~ state.id, prevState.id", state.id, prevState.id) // 此处log id是一样的, 原因是返回都是同一个对象的引用, 除非用clonedeep
+        /* ... */
+      },
+      {
+        deep: true,
+      }
+    )
+
+    // setTimeout(() => {
+    //   resWatch()
+    // }, 2500);
     return {
       count: count,
       increment,
+      nested: {
+        count
+      },
+      nested2: reactive({
+        count,
+        },
+      ),
+      state
     };
   },
   data() {
     return { msg: "setup demo" };
   },
   mounted() {
-    setInterval(() => {
+    let count = 0
+    const timer = setInterval(() => {
       this.increment();
+      this.state.id = Math.random()
+      count++
+      if(count >= 3){
+        clearTimeout(timer)
+      }
+
     }, 1000);
+    console.log("🚀 ~ timer ~ timer", timer)
   },
   methods: {
+    log(){
+      console.log('this.count', this.count)
+    }
     // increment() {
     //   console.log("in");
     //   this.count++;
@@ -36,4 +108,14 @@ export default defineComponent({
 });
 </script>
 
-<template>Counter: {{ count }}</template>
+<template>
+<div>
+  <p>Counter: {{ count }} {{log && log()}}</p>
+  <button @click="count ++">Increment count</button>
+  <br>
+  <button @click="nested.count.value++">ref -- Nested Increment count</button>
+  <br>
+  <button @click="nested2.count++">reactive -- Nested Increment count</button>
+
+</div>
+</template>
